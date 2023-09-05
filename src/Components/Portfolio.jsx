a3c2d70e0c5cb6fadc8fdad3c4de46c4c44267bf
Sajath Firthows <106432527+sajathfirthows@users.noreply.github.com>
2023-09-06 02:27:22 +0530

    add all cahnges

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Rainbow.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "rainbow image";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Unlocking Seamless Collaboration and Efficiency with Azure DevOps",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Unlock the potential for seamless collaboration and enhanced efficiency in your software development process with Azure DevOps.",
+    url: "https://medium.com/@sajathfirthows/unlocking-seamless-collaboration-and-efficiency-with-azure-devops-dfb62236119a",
   },
   {
-    title: "Web Development for Beginners",
+    title: "GitHub Actions Unleashed: Mastering Automation for Developers",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Dive into the world of GitHub Actions and harness the power of automation to become a masterful developer.",
+    url: "https://medium.com/@sajathfirthows/github-actions-unleashed-mastering-automation-for-developers-cee3088a59b5",
   },
   {
-    title: "My Resume Site",
+    title: "Building Docker Image on Azure VM and Pushing to Docker Hub",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Building Docker Image on Azure VM and Pushing to Docker Hub.",
+    url: "https://github.com/sajathfirthows/DevOps_Projects/tree/master/Building%20Docker%20Image%20on%20Azure%20VM%20and%20Pushing%20to%20Docker%20Hub",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Installing Jenkins on EC2 with custom port",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Installing Jenkins on EC2 with custom port.",
+    url: "https://github.com/sajathfirthows/DevOps_Projects/tree/master/Installing%20Jenkins%20on%20EC2%20with%20custom%C2%A0port",
   },
 ];
 
